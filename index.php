79599de207ec0db1b0a9729cc28cd0b338fa8392
MylAnt95 <anton.myllykoski95@gmail.com>
2023-09-28 16:01:43 +0200

    Created all files necessary for the assignment and i also added the first row(categories) in the table

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require __DIR__ . '/header.php';
+require __DIR__ . '/data.php';
+?>
+
+<body>
+    <div class="container">
+        <!--Table with categories in first row-->
+        <table>
+            <tr>
+                <th>Team</th>
+                <th>League</th>
+                <th>City</th>
+                <th>Last time champions</th>
+            </tr>
+            <!--Link to a PHP file that loops through the categories-->
+        </table>
+    </div>
+</body>
+</html>
