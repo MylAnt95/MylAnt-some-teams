0685de0dced55ab39fb8bbf8de61b128567c7fe5
MylAnt95 <anton.myllykoski95@gmail.com>
2023-10-02 13:28:08 +0200

    Added count to teams-total.php and echo total teams to index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 require __DIR__ . '/header.php';
+require __DIR__ . '/teams-total.php';
 ?>
 
 <body>
     <div class="container">
         <!--Table with categories in the first row-->
         <table>
+            <p>Total teams <?=$teamCount?></p>          
             <tr>
                 <th>Team</th>
                 <th>League</th>
---
+++ teams-total.php
@@ -0,0 +1,6 @@
+<?php
+require __DIR__ . '/data.php';
+
+$teamCount = count($teams);
+
+?>
